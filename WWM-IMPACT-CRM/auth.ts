40f74531a0b083ba7a83f80a16a2ae7e2f0ae6b0
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-10-22 18:20:19 -0500

    Installed magic links

--- WWM-IMPACT-CRM/auth.ts
+++ WWM-IMPACT-CRM/auth.ts
@@ -1,4 +1,5 @@
 import { betterAuth } from "better-auth";
+import { magicLink } from "better-auth/plugins";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "./node_modules/@prisma/client";
 
@@ -8,4 +9,11 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "sqlite",
   }),
+  plugins: [
+    magicLink({
+        sendMagicLink: async ({ email, token, url }, request) => {
+
+        },
+    })
+  ],
 });
