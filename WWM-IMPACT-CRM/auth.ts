5e9255f9b721f42af6fc1e4a173f997b6ae585eb
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-10-29 02:19:33 -0500

    Configuring nodemailer and email sending

--- WWM-IMPACT-CRM/auth.ts
+++ WWM-IMPACT-CRM/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { magicLink } from "better-auth/plugins";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "./node_modules/@prisma/client";
+import nodemailer from "nodemailer";
 
 const prisma = new PrismaClient();
 
@@ -12,7 +13,20 @@ export const auth = betterAuth({
   plugins: [
     magicLink({
         sendMagicLink: async ({ email, token, url }, request) => {
+          const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: process.env.EMAIL_USER,
+              pass: process.env.EMAIL_PASS,
+            },
+          });
 
+          await transporter.sendMail({
+            from: '"WalkWithMe.global" <login@walkwithme.global>',
+            to: email,
+            subject: "WalkWithMe.global Login Link",
+            html: `<a href=${url}>Log in!</a>`,
+          });
         },
     })
   ],
