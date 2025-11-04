90adfb00390c10e6998a683b7238e1b37bfc86eb
zarasadeq16-hue <zarasadeq16@gmail.com>
2025-11-04 16:37:45 -0600

    fundapi

--- WWM-IMPACT-CRM/prisma-rest-api/server.ts
+++ WWM-IMPACT-CRM/prisma-rest-api/server.ts
@@ -4,16 +4,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import reportAPI from './reportAPI'; 
 import campaignAPI from './campaignAPI';
+import fundAPI from './fundAPI';
+
 
 
 const app = express();
 const PORT = 3000;
 
+//call report api
 app.use(bodyParser.json());
 app.use('/api', reportAPI);
 
+//call campaign api
 app.use(bodyParser.json());
 app.use('/api', campaignAPI);
+
+//call fund api
+app.use(bodyParser.json());
+app.use('/api', fundAPI);
+
+
 
 app.get('/', (req, res) => {
   res.send('API is working!');
