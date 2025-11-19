ac318f842848435103da3ce382d87a02e74c3b1f
zarasadeq16-hue <zarasadeq16@gmail.com>
2025-11-19 16:18:35 -0600

    create crossfund.vue

---
+++ WWM-IMPACT-CRM/app/pages/crossfund.vue
@@ -0,0 +1,1 @@
+<iframe title="cross-fund-Dashboard" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODFhZTlkYjAtZDg5Zi00NjgyLWFhMmEtNjcyMjc5YTliZTUxIiwidCI6IjhkMjgxZDFkLTljNGQtNGJmNy1iMTZlLTAzMmQxNWRlOWY2YyIsImMiOjN9" frameborder="0" allowFullScreen="true"></iframe>
