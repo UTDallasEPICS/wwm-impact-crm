d819be05504ebc5d100c51372c58c831a3b7cf4a
zarasadeq16-hue <zarasadeq16@gmail.com>
2025-11-22 18:59:58 -0600

    add donor.vue

---
+++ WWM-IMPACT-CRM/app/pages/donor.vue
@@ -0,0 +1,5 @@
+<template>
+    <div >
+    <iframe title="donor-Dashboard" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZGIyZTA4OTItN2FiYy00MDBkLWI3YmYtNjkwZjFiMWUxYjYxIiwidCI6IjhkMjgxZDFkLTljNGQtNGJmNy1iMTZlLTAzMmQxNWRlOWY2YyIsImMiOjN9" frameborder="0" allowFullScreen="true"></iframe>
+    </div>
+</template>
