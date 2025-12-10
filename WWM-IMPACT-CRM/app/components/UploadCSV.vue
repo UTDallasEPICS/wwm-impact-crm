38e373b97a758cba82a22dd7bf3fcd52d13770c7
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-12-09 20:56:03 -0600

    Began ingestion script

--- WWM-IMPACT-CRM/app/components/UploadCSV.vue
+++ WWM-IMPACT-CRM/app/components/UploadCSV.vue
@@ -5,7 +5,7 @@
       <input
         type="file"
         multiple
-        accept=".csv"
+        accept=".csv,.xslx"
         @change="onFileChange"
         class="file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
       />
