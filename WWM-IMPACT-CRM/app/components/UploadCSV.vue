4653e5e3b10d339236b45461569a4e744744e8fb
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2025-12-10 13:08:28 -0600

    Upload form now accepts XLSX (some sample data sheets were in XLSX format).

--- WWM-IMPACT-CRM/app/components/UploadCSV.vue
+++ WWM-IMPACT-CRM/app/components/UploadCSV.vue
@@ -5,7 +5,7 @@
       <input
         type="file"
         multiple
-        accept=".csv,.xslx"
+        accept=".csv,.xlsx,.xls,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         @change="onFileChange"
         class="file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
       />
